6bf10074e189f44d0fda65c60ad39da67b06ab42
lvl0 <106563820+lvl0-io@users.noreply.github.com>
2024-01-21 21:54:45 -0600

    Update GetTokenBalance.js

--- src/GetTokenBalance.js
+++ src/GetTokenBalance.js
@@ -7,7 +7,7 @@ const { abi } = require("./EthVault.json");
 // Replace with your token contract's address
 const tokenAddress = process.env.CONTRACT_ADDRESS;
 
-async function swapUSDC() {
+async function getTokenBalance() {
   const signer = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY);
 
   const contract = new web3.eth.Contract(abi, tokenAddress);
@@ -20,4 +20,4 @@ async function swapUSDC() {
   return swap;
 }
 
-swapUSDC().then((swap) => console.log(swap));
+getTokenBalance().then((swap) => console.log(swap));
